08afa21f94acb152f5c56021e1feaa5f6c0029d0
Allen <allendinghamilton@gmail.com>
2017-07-29 13:05:34 -0400

    first version

---
+++ farming-with-autoplay.sikuli/farming-with-autoplay.py
@@ -0,0 +1,17 @@
+
+while 1: 
+    wait("1501346151543.png", 10)
+    
+    doubleClick("1501346151543.png")
+    wait("1501346228539.png", 30)
+    click("1501346228539.png")
+    
+    wait(Pattern("1501346620416.png").similar(0.30), 300)
+    doubleClick(Pattern("1501346620416.png").similar(0.30))
+    wait("1501346685927.png", 10)
+    doubleClick("1501346702300.png")
+    wait("1501346821874.png", 10)
+    click("1501346821874.png")
+    wait("1501346890146.png", 10)
+    doubleClick("1501346907635.png")
+    
