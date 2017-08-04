5f9f5932931d3ca4ed55de24298627e14e14fe72
Allen <allendinghamilton@gmail.com>
2017-08-04 04:35:09 -0400

    added logic for level up

--- farming-with-autoplay.sikuli/farming-with-autoplay.py
+++ farming-with-autoplay.sikuli/farming-with-autoplay.py
@@ -10,6 +10,9 @@ while 1:
     doubleClick(Pattern("1501397974355.png").similar(0.50))
     wait(Pattern("1501398033620.png").similar(0.85), 20)
     doubleClick("1501398033620.png")
+    
+    
+    # wait for another next in LB trials  
     wait(20)
     if exists(Pattern("1501398033620.png").similar(0.85)):
         doubleClick(Pattern("1501398033620.png").similar(0.85))
@@ -17,6 +20,9 @@ while 1:
     click("1501398061521.png")
     wait("1501398090920.png", 20)
     doubleClick("1501398090920.png")
+
+    
+    # refill stamina if needed
     while exists(Pattern("1501399449365.png").targetOffset(60,80)):
         doubleClick(Pattern("1501399449365.png").targetOffset(60,80))
         wait(10)
@@ -24,8 +30,12 @@ while 1:
       
             doubleClick(Pattern("1501649717261.png").targetOffset(-9,-1))
             wait(10)
-        wait(5)
-        #wait("1501398090920.png", 20)  
-        #doubleClick("1501398090920.png")
+
+    
+    # check for level up stamina refill
+    wait(10)
+    if exists("1501834764766.png"):
+        doubleClick("1501834780905.png")
+        doubleClick("1501834963337.png")
    
     
